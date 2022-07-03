b17de4f59a8f2c2f0083e86b63fa903cf882d0db
clipesr <106500365+clipesr@users.noreply.github.com>
2022-07-03 16:46:03 +0300

    Update ping.py

--- R0R77/plugins/ping.py
+++ R0R77/plugins/ping.py
@@ -5,12 +5,12 @@ from telethon import Button, events
 from R0R77 import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/2ad68bd0e391a69163d0a.jpg"
+    "PING_PIC", "https://telegra.ph/file/7e896dba43e7e25106e4e.jpg"
 )
 ms = 4
 
 ALIVE = os.environ.get(
-    "ALIVE", "@R0R77"
+    "ALIVE", "@HssHH"
 )
 
 CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
 
 @R0R77.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    UMM = [[Button.url("السورس", "https://t.me/JMTHON")]]
+    UMM = [[Button.url("Hasoni Alnajar", "https://t.me/hsshh")]]
     await R0R77.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
