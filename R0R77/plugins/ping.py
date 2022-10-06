5d3a98078bd42adbe2d31c02fa853957a9154a76
- Haidar <103788745+HHvHH@users.noreply.github.com>
2022-10-06 22:32:20 +0300

    Update ping.py

--- R0R77/plugins/ping.py
+++ R0R77/plugins/ping.py
@@ -5,12 +5,12 @@ from telethon import Button, events
 from R0R77 import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/9381a140aafbb75177300.jpg"
+    "PING_PIC", "https://telegra.ph/file/16c8c27938bc7ac235436.jpg"
 )
 ms = 4
 
 ALIVE = os.environ.get(
-    "ALIVE", "@HssHH"
+    "ALIVE", "@HvvHH"
 )
 
 CAPTION = f"**سرعة البنق :** {ms}"
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنق :** {ms}"
 
 @R0R77.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    UMM = [[Button.url("Hasoni Alnajar", "https://t.me/hsshh")]]
+    UMM = [[Button.url("Haider .", "https://t.me/HvvHH")]]
     await R0R77.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
