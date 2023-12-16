5d0a8e669078878e258841a6dbbbb59a8a355f73
- Haidar <103788745+HHvHH@users.noreply.github.com>
2023-12-16 16:24:52 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "19351645"))
-    API_HASH = os.environ.get("API_HASH", "359f66a067069b8b5c482df6a8a4e3b8")
+    API_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "o7Tbot")
-    SUPPORT = os.environ.get("SUPPORT", "DEOOU")
-    CHANNEL = os.environ.get("CHANNEL", "DEOOU")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    SUPPORT = os.environ.get("SUPPORT", "HvvHH")
+    CHANNEL = os.environ.get("CHANNEL", "HvvHH")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/16c8c27938bc7ac235436.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/16c8c27938bc7ac235436.jpg")
